e49c85865150a6fdfeb3e41b21f566147011383f
GiftBless <twinsyt098@gmail.com>
2021-05-12 14:13:33 +0530

    AllForChrist

---
+++ app.js
@@ -0,0 +1,22 @@
+function foo() {
+   alert("Thank You For Visiting");
+   return true;
+   
+   
+}
+   function ScrollToOdisha(){
+      window.scrollBy(0,1000);
+
+   }
+   function ScrollToLiveperformance(){
+      window.scrollBy(0,2550);
+
+   }
+   function ScrollTocontact(){
+      window.scrollBy(0,4000);
+
+   }
+   function ScrollTohome(){
+      window.scrollBy(0,-4000);
+
+   }
